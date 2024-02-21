b1af35a7c04978c6d7fa5d8787e7e60a0e371bd5
abbas <abbasmomeny1994@gmail.com>
2024-02-21 06:22:00 +0330

    add: ping check route

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/abbasfisal/go-api/helper"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("start server")
 
-	helper.PanicIfError(http.ListenAndServe("localhost:8080", nil))
+	router := httprouter.New()
+	router.GET("/ping", pingHandler)
+
+	helper.PanicIfError(http.ListenAndServe("localhost:8080", router))
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Fprintf(w, "ping check")
 }
