2192cff7d232e85ffaf53ffd1f3c7733472624e4
abbas <abbasmomeny1994@gmail.com>
2024-02-21 06:38:27 +0330

    add: database connection

---
+++ config/database.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/abbasfisal/go-api/helper"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "root"
+	password = "password"
+	dbName   = "test"
+)
+
+func DatabaseConnection() *sql.DB {
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable ", host, port, user, password, dbName)
+	db, err := sql.Open("postgres", sqlInfo)
+	helper.PanicIfError(err)
+
+	log.Info().Msg("Database Connected !")
+
+	return db
+}
